f1f0c1dedf2b45371844703433e083b81b0bf28e
kenluiken <md20051179@gmail.com>
2019-10-23 13:59:27 +0800

    viewhtml completed

--- djangoProject/BackTrack/pbi/views.py
+++ djangoProject/BackTrack/pbi/views.py
@@ -1,6 +1,25 @@
 from django.views.generic import TemplateView
 from pbi.models import Item
+from pbi.forms import ItemForm
 
+def newPBI(request):
+	template_name = 'pbi_new.html'
+	form = ItemForm(request.POST or None)
+	
+	if form.is_valid():
+		obj = Item.objects.create(
+			order = form.cleaned_data.get('order'),
+			name = form.cleaned_data.get('name'),
+			description = form.cleaned_data.get('description'),
+			original_sprint_size = form.cleaned_data.get('original_sprint_size'),
+			remaining_sprint_size = form.cleaned_data.get('remaining_sprint_size'),
+			estimate_of_story_point = form.cleaned_data.get('estimate_of_story_point'),
+			status = form.cleaned_data.get('status')
+		)
+		return HttpResponse('OK')
+	return render(request, template_name, context={'form':form})
+		
+		
 class PbiView(TemplateView):
       template_name = 'pbi_list.html'
 
